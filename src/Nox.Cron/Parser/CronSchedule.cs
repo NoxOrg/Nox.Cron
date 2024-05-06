7cb16686b9ebec04d6a0320c36e2ee6d1f6ba79b
jan <janschutte.lands@gmail.com>
2024-05-06 12:13:17 +0200

    - added netstandard2.0 support

--- src/Nox.Cron/Parser/CronSchedule.cs
+++ src/Nox.Cron/Parser/CronSchedule.cs
@@ -6,12 +6,12 @@
         {
         }
 
-        public string Minutes { get; init; } = string.Empty;
-        public string Hours { get; init; } = string.Empty;
-        public string DayOfMonth { get; init; } = string.Empty;
-        public string Months { get; init; } = string.Empty;
-        public string DayOfWeek { get; init; } = string.Empty;
-        public string Unparsed { get; init; } = string.Empty;
+        public string Minutes { get; set; } = string.Empty;
+        public string Hours { get; set; } = string.Empty;
+        public string DayOfMonth { get; set; } = string.Empty;
+        public string Months { get; set; } = string.Empty;
+        public string DayOfWeek { get; set; } = string.Empty;
+        public string Unparsed { get; set; } = string.Empty;
 
         public override string ToString() => $"{Minutes} {Hours} {DayOfMonth} {Months} {DayOfWeek}";
         public bool IsFullyParsed() => string.IsNullOrWhiteSpace(Unparsed);
